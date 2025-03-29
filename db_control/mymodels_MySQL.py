2f1a93b46cbb3307eb5be7321fc4de88960165be
fuku-git-423 <ay.fuku.star@gmail.com>
2025-03-29 21:32:52 +0900

    mymodels_MySQLの型定義を変更

--- db_control/mymodels_MySQL.py
+++ db_control/mymodels_MySQL.py
@@ -1,11 +1,26 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, JSON, TIMESTAMP, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 # from datetime import datetime
-
 
 class Base(DeclarativeBase):
     pass
 
+class HairQuality(Base):
+    __tablename__ = 'hair_quality'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    nickname: Mapped[str] = mapped_column(String(255))
+    age: Mapped[int] = mapped_column(Integer)
+    gender: Mapped[str] = mapped_column(String(20))
+    density: Mapped[str] = mapped_column(String(50))
+    hair_loss: Mapped[str] = mapped_column(String(50))
+    scalp: Mapped[dict] = mapped_column(JSON)
+    thickness: Mapped[str] = mapped_column(String(50))
+    texture: Mapped[dict] = mapped_column(JSON)
+    firmness: Mapped[str] = mapped_column(String(20))
+    created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now())
+    updated_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP, server_default=func.now(), onupdate=func.now()
+    )
 
 class Customers(Base):
     __tablename__ = 'customers'
@@ -13,25 +28,3 @@ class Customers(Base):
     customer_name: Mapped[str] = mapped_column(String(100))
     age: Mapped[int] = mapped_column(Integer)
     gender: Mapped[str] = mapped_column(String(10))
-
-
-class Items(Base):
-    __tablename__ = 'items'
-    item_id: Mapped[str] = mapped_column(String(10), primary_key=True)
-    item_name: Mapped[str] = mapped_column(String(100))
-    price: Mapped[int] = mapped_column(Integer)
-
-
-class Purchases(Base):
-    __tablename__ = 'purchases'
-    purchase_id: Mapped[str] = mapped_column(String(10), primary_key=True)
-    customer_id: Mapped[str] = mapped_column(String(10))
-    purchase_date: Mapped[str] = mapped_column(String(10))
-
-
-class PurchaseDetails(Base):
-    __tablename__ = 'purchase_details'
-    detail_id: Mapped[str] = mapped_column(String(10), primary_key=True)
-    purchase_id: Mapped[str] = mapped_column(String(10))
-    item_id: Mapped[str] = mapped_column(String(10))
-    quantity: Mapped[int] = mapped_column(Integer)
